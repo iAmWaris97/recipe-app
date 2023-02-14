56d5f4f709ae83ee124969212bc1fc5f6d6317a3
iAmWaris97 <haleem.waris@gmail.com>
2023-02-14 20:11:08 +0430

    Generated users and inventory migrations

---
+++ test/controllers/inventories_controller_test.rb
@@ -0,0 +1,48 @@
+require "test_helper"
+
+class InventoriesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @inventory = inventories(:one)
+  end
+
+  test "should get index" do
+    get inventories_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_inventory_url
+    assert_response :success
+  end
+
+  test "should create inventory" do
+    assert_difference("Inventory.count") do
+      post inventories_url, params: { inventory: { Name: @inventory.Name } }
+    end
+
+    assert_redirected_to inventory_url(Inventory.last)
+  end
+
+  test "should show inventory" do
+    get inventory_url(@inventory)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_inventory_url(@inventory)
+    assert_response :success
+  end
+
+  test "should update inventory" do
+    patch inventory_url(@inventory), params: { inventory: { Name: @inventory.Name } }
+    assert_redirected_to inventory_url(@inventory)
+  end
+
+  test "should destroy inventory" do
+    assert_difference("Inventory.count", -1) do
+      delete inventory_url(@inventory)
+    end
+
+    assert_redirected_to inventories_url
+  end
+end
