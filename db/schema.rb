e9e283543e3ce6441cf355f078e21567eabf6b5a
iAmWaris97 <haleem.waris@gmail.com>
2023-02-14 20:13:22 +0430

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_14_153401) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_14_153832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "inventories", force: :cascade do |t|
+    t.string "Name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
