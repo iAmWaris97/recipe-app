fe49fbfbe692e00fb2efc0ced169b44531aa6ac0
iAmWaris97 <haleem.waris@gmail.com>
2023-02-14 22:52:24 +0430

    run migration for recipe foods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_14_164711) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_14_181440) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_164711) do
     t.index ["inventory_id"], name: "index_inventory_foods_on_inventory_id"
   end
 
+  create_table "recipe_foods", force: :cascade do |t|
+    t.string "quantity"
+    t.bigint "food_id", null: false
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
+  end
+
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.integer "preparation_time"
@@ -61,5 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_164711) do
   add_foreign_key "inventories", "users"
   add_foreign_key "inventory_foods", "foods"
   add_foreign_key "inventory_foods", "inventories"
+  add_foreign_key "recipe_foods", "foods"
+  add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
 end
