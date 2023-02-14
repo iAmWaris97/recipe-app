a4929a87697aa5e1cee73bdaeb631a9c46fedb70
iAmWaris97 <haleem.waris@gmail.com>
2023-02-14 21:10:38 +0430

    Create foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :foods
   resources :recipes
   resources :inventories
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_14_155213) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_14_163643) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "inventories", force: :cascade do |t|
     t.string "Name"
