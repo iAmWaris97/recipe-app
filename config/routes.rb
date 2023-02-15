cd319aec4d4928a201830e49fe213c4b5923a217
iAmWaris97 <haleem.waris@gmail.com>
2023-02-15 22:35:42 +0430

    install devise configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :recipe_foods
   resources :inventory_foods
   resources :foods
-  resources :recipes
+  resources :recipes, only: [:index] do
+  end
   resources :inventories
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "foods#index"
+  root "recipes#index"
 end
