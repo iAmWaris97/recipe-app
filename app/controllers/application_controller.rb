31c78b2ed8faa7139bac608cd1387be6863155fa
erickhansx <erickhansx@gmail.com>
2023-02-15 16:21:06 -0600

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
   private
 
   def after_sign_out_path_for(*)
