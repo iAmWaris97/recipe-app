df61c12610f0c8f2606b53d446fce16333553946
ndorrh <ndorrh@gmail.com>
2023-02-16 11:16:25 +0100

    Add authorization for recipe controller

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,9 @@ class Ability
   include CanCan::Ability
 
 def initialize(user)
-    can [:read, :public, :generate_shopping], Recipe   # start by defining rules for all users, also not logged ones
+    can [:read, :public, :generate_shopping], Recipe  # start by defining rules for all users, also not logged ones
     return unless user.present?
-    can [:create], Recipe
-    can [:destroy, :update], Recipe, user_id: user.id # if the user is logged in can manage it's own recipe
+    can [:create, :destroy, :update], Recipe, user_id: user.id # if the user is logged in can manage it's own recipe
     can [:read, :create], Food # logged in users can also create comments
 end
 end
