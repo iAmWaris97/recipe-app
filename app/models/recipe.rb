a722f3acba319006f0bb685130a452b7c17a8f3f
iAmWaris97 <haleem.waris@gmail.com>
2023-02-17 00:02:50 +0430

    Add assocaiations and validations for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :delete_all
+
+  validates :name, :preparation_time, :cooking_time, :description, presence: true
 end
