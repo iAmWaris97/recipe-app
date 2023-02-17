1d94e10912af4183cfc789eb47716d0da46f18d2
erickhansx <erickhansx@gmail.com>
2023-02-17 14:39:28 -0600

    Update inventory.rb

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,11 +1,11 @@
 class ShoppingListsController < ApplicationController
-    def index
-      @recipe_foods = RecipeFood.includes(:food, :recipe).where(recipe_id: params[:recipe_id])
-      @recipe_name = Recipe.find(params[:recipe_id])
-      @inventory_name = Inventory.find(params[:inventory_id])
-      @inventory_foods = InventoryFood.where(inventory_id: params[:inventory_id])
-      @recipes = Recipe.find(params[:recipe_id])
-      @inventory = Inventory.find(params[:inventory_id])
-      @general_shopping = Recipe.find(params[:recipe_id])
-    end
+  def index
+    @recipe_foods = RecipeFood.includes(:food, :recipe).where(recipe_id: params[:recipe_id])
+    @recipe_name = Recipe.find(params[:recipe_id])
+    @inventory_name = Inventory.find(params[:inventory_id])
+    @inventory_foods = InventoryFood.where(inventory_id: params[:inventory_id])
+    @recipes = Recipe.find(params[:recipe_id])
+    @inventory = Inventory.find(params[:inventory_id])
+    @general_shopping = Recipe.find(params[:recipe_id])
+  end
 end
