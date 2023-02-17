1f2f209af4c44cd1f5e4e8d291209bbb37766208
ndorrh <ndorrh@gmail.com>
2023-02-17 01:07:44 +0100

    Add test for Inventory model

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Inventory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {Inventory.new(user_id: 1, Name: 'inventory 1')}
+  before { subject.save }
+
+  it 'Food Name should be present' do
+    subject.Name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Name should be inventory 1' do
+    expect(subject.Name).to eql 'inventory 1'
+  end
 end
